ec2b1045e19b65f1e41a043a814dd700f0aa6311
Magacu <melendomorado@gmail.com>
2025-11-27 17:53:17 +0100

    Probability chuletario

---
+++ PROBABILITY.R
@@ -0,0 +1,11 @@
+#CONDITIONAL PROBABILITY
+#PCon (A|B) = P(AnB)/P(B)
+
+#INDEPENDENCE OF EVENTS
+#Independent if PCon(A,B) = P(A)
+
+#Total Probability
+#P(B) = SUM(PAi)/P(B|Ai)
+
+#Bayes Theorem
+#P(Ai|B)= P(Ai n B)/P(B) = P(Ai)*P(B|Ai)/P(Ai)*P(B|Ai)
